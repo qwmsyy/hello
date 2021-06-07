2e4c876178d93dc45755b02c6532ebe3b9b9c4b6
Ms <18437909932@163.com>
2021-06-07 11:56:52 +0800

    vue basic content

---
+++ text/.vue
@@ -0,0 +1,53 @@
+/*
+ * alias: {
+ *      '@': resolve('src'),
+ *      '@config': resolve('config'),
+ *      'vue$': 'vue/dist/vue.common.js'
+ *  }
+*/
+<template>
+	<div class="ms-container">
+	</div>
+</template>
+
+<script>
+	import {**,*} from '' // @  webpack配置
+	
+	export default{
+		name: 'Hello',
+		data() {
+			return {
+				msg: 'ok',
+			}
+		},
+		computed: { // 支持缓存 不支持异步
+			reverseMsg:function() {
+				return this.msg.split('').reverse().join('')
+			}
+		},
+		watch: {
+			msg: {
+				get: function() {
+					return 'get' + this.msg
+				}
+				set: function() {
+					return 'set' + this.msg
+				}
+			}
+		},
+		created: {
+			this.init()
+		},
+		methods: {
+			init() {
+				console.log("hello world ,init ")
+			},
+		},
+	}
+</script>
+
+<style>
+	.ms-container{
+		padding: 5px;
+	}
+</style>
